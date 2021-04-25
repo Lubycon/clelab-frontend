4d6230568a0864f84333891f2d4e467c49f8ad79
s-ong-c <somony9292@gmail.com>
2021-04-25 17:29:04 +0900

    feat: globalStyle & font file update

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,70 @@
+import { css, Global } from '@emotion/react'
+
+export const GlobalStyle = () => (
+  <Global
+    styles={[
+      css`
+        * {
+          box-sizing: inherit;
+        }
+        body {
+          background-color: #ffffff;
+          margin: 0;
+          padding: 0;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          font-family: 'Noto Sans KR';
+        }
+        html,
+        body,
+        #root {
+          height: 100%;
+          font-size: 100%;
+        }
+        a {
+          text-decoration: none;
+        }
+      `,
+      baseFont,
+    ]}
+  />
+)
+
+const baseFont = css`
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/NotoSansKR-Regular.woff2') format('woff2'),
+      url('/fonts/NotoSansKR-Regular.woff') format('woff'),
+      url('/fonts/NotoSansKR-Regular.otf') format('opentype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 500;
+    src: url('/fonts/NotoSansKR-Medium.woff2') format('woff2'),
+      url('/fonts/NotoSansKR-Medium.woff') format('woff'),
+      url('/fonts/NotoSansKR-Medium.otf') format('opentype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 700;
+    src: url('/fonts/NotoSansKR-Bold.woff2') format('woff2'),
+      url('/fonts/NotoSansKR-Bold.woff') format('woff'),
+      url('/fonts/NotoSansKR-Bold.otf') format('opentype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 800;
+    src: url('/fonts/NotoSansKR-Bold.woff2') format('woff2'),
+      url('/fonts/NotoSansKR-Bold.woff') format('woff'),
+      url('/fonts/NotoSansKR-Bold.otf') format('opentype');
+    font-display: swap;
+  }
+`
