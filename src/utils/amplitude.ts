d8b01fc80d973b42524f7e295e04f0c7ab090834
evan-moon <bboydart91@gmail.com>
2021-05-22 18:14:42 +0900

    feat: logger 모듈 추가 및 세팅

---
+++ src/utils/amplitude.ts
@@ -0,0 +1,26 @@
+import { AmplitudeClient } from 'amplitude-js'
+
+const API_KEY = process.env.AMPLITUDE_API_KEY
+let amplitudeClient: AmplitudeClient | null = null
+let initialized = false
+
+export const getAmplitudeClient = (): Promise<AmplitudeClient | null> => {
+  return new Promise((resolve) => {
+    if (initialized) {
+      resolve(amplitudeClient)
+
+      return
+    }
+
+    require('amplitude-js').init(
+      API_KEY,
+      'unknown',
+      {},
+      async (client: AmplitudeClient) => {
+        initialized = true
+        amplitudeClient = client
+        resolve(client)
+      },
+    )
+  })
+}
