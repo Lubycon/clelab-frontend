6627d0c75e4f946849394a29299b1c308f8ab58f
evan-moon <bboydart91@gmail.com>
2021-07-30 17:40:08 +0900

    chore: lubycon 관련 라이브러리 업데이트

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -1,13 +1,15 @@
-import { FirebaseConfig } from '@lubycon/utils'
+import { FirebaseConfig } from '@lubycon/logger'
 
 import { isProduction } from './env'
 
 export const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '123123',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '1231231',
   projectId: isProduction ? 'clelab' : 'logger-library-test',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_HOST ?? '',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID ?? '',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_HOST ?? '123123',
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID ?? '123123',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '123123',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '12312312',
 }
