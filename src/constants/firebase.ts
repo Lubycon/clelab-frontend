9065a6a9f140b864a8e6d757f884892897de217c
evan-moon <bboydart91@gmail.com>
2021-07-30 17:40:57 +0900

    fix: 테스트용 값 제거

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -3,13 +3,11 @@ import { FirebaseConfig } from '@lubycon/logger'
 import { isProduction } from './env'
 
 export const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '123123',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '1231231',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
   projectId: isProduction ? 'clelab' : 'logger-library-test',
-  storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_HOST ?? '123123',
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID ?? '123123',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '123123',
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '12312312',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_HOST ?? '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID ?? '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
 }
